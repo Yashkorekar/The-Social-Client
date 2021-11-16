97d4f302411955552200f59d6dc5dbe4383bfd48
Dhiraj <jdhiraj543@gmail.com>
2021-11-16 21:19:15 +0530

    Added Context Switches

---
+++ src/context/AuthContext.js
@@ -0,0 +1,25 @@
+import AuthReducer from "./AuthReducer";
+import { createContext, useReducer } from "react";
+const INTITIAL_STATE = {
+    user: null,
+    isFetching: false,
+    error: false
+}
+
+export const AuthContext = React.createContext(INTITIAL_STATE);
+
+
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+    return (
+        < AuthContext.Provider
+            value={{
+                user: state.user,
+                isFetching: state.isFetching,
+                error: state.error,
+                dispatch
+            }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
---
+++ src/context/AuthReducer.js
@@ -0,0 +1,23 @@
+const AuthReducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN_START":
+            return {
+                user: null,
+                isFecthing: true,
+                error: false
+            }
+        case "LOGIN_SUCCESS":
+            return {
+                user: action.payload,
+                isFecthing: false,
+                error: false
+            }
+        case "LOGIN_FAILURE":
+            return {
+                user: null,
+                isFecthing: false,
+                error: action.payload
+            }
+    }
+}
+export default AuthReducer;
