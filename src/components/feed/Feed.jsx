d66dc1a2ce0a69bcea8bc5ca4e47ceef221b5c8c
Dhiraj <jdhiraj543@gmail.com>
2021-11-19 07:56:40 +0530

    updated post, feed, added msg

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,30 +1,26 @@
-import "./feed.css";
-import React from "react";
-import Share from "../share/Share";
+import { useContext, useEffect, useState } from "react";
 import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
 import axios from "axios";
-import { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
-function Feed({ username }) {
+export default function Feed({ username }) {
   const [posts, setPosts] = useState([]);
   const { user } = useContext(AuthContext);
 
-
-  //get posts from server
   useEffect(() => {
-    const fetchposts = async () => {
-      const response = username
-        ? await axios.get("/post/profile/" + username)
-        : await axios.get("/post/timeline/" + user._id);
-      // arranging the posts based on the created at time in descending order
+    const fetchPosts = async () => {
+      const res = username
+        ? await axios.get("/posts/profile/" + username)
+        : await axios.get("posts/timeline/" + user._id);
       setPosts(
-        response.data.sort((p1, p2) => {
+        res.data.sort((p1, p2) => {
           return new Date(p2.createdAt) - new Date(p1.createdAt);
         })
       );
     };
-    fetchposts();
+    fetchPosts();
   }, [username, user._id]);
 
   return (
@@ -38,5 +34,3 @@ function Feed({ username }) {
     </div>
   );
 }
-
-export default Feed;
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,19 +1,16 @@
-import { MoreVert } from "@material-ui/icons";
 import "./post.css";
-import React from "react";
+import { MoreVert } from "@material-ui/icons";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import { useState, useEffect, useContext } from "react";
+import { format } from "timeago.js";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
-// time ago
-import { format } from "timeago.js";
-
-function Post({ post }) {
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+export default function Post({ post }) {
   const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
   const [user, setUser] = useState({});
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user: currentUser } = useContext(AuthContext);
 
   useEffect(() => {
@@ -22,15 +19,15 @@ function Post({ post }) {
 
   useEffect(() => {
     const fetchUser = async () => {
-      const response = await axios.get(`/user?userId=${post.userId}`);
-      setUser(response.data);
+      const res = await axios.get(`/users?userId=${post.userId}`);
+      setUser(res.data);
     };
     fetchUser();
   }, [post.userId]);
 
   const likeHandler = () => {
     try {
-      axios.put("/post/" + post._id + "/like", { userId: currentUser._id });
+      axios.put("/posts/" + post._id + "/like", { userId: currentUser._id });
     } catch (err) {}
     setLike(isLiked ? like - 1 : like + 1);
     setIsLiked(!isLiked);
@@ -40,15 +37,18 @@ function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            {/* use a noavatar profile here */}
             <Link to={`/profile/${user.username}`}>
               <img
                 className="postProfileImg"
-                src={user.profilePicture || PF+"profile/noProfile.png"}
+                src={
+                  user.profilePicture
+                    ? PF + user.profilePicture
+                    : PF + "person/noAvatar.png"
+                }
                 alt=""
               />
             </Link>
-            <span className="postUserName">{user.username}</span>
+            <span className="postUsername">{user.username}</span>
             <span className="postDate">{format(post.createdAt)}</span>
           </div>
           <div className="postTopRight">
@@ -56,26 +56,24 @@ function Post({ post }) {
           </div>
         </div>
         <div className="postCenter">
-          {/* ? for some of the post dont have description */}
           <span className="postText">{post?.desc}</span>
-          {/* post image */}
-          <img src={PF+post.img} alt="" className="postImg" />
+          <img className="postImg" src={PF + post.img} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img
-              src={`${PF}/profile/heart.png`}
-              alt=""
-              onClick={likeHandler}
               className="likeIcon"
+              src={`${PF}like.png`}
+              onClick={likeHandler}
+              alt=""
             />
             <img
-              src={`${PF}/profile/like.png`}
-              alt=""
-              onClick={likeHandler}
               className="likeIcon"
+              src={`${PF}heart.png`}
+              onClick={likeHandler}
+              alt=""
             />
-            <span className="postLikeCounter">{like}</span>
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
@@ -85,5 +83,3 @@ function Post({ post }) {
     </div>
   );
 }
-
-export default Post;
