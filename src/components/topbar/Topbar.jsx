e3c323d426a9833c8146cc791b08354ba3aae4b6
Dhiraj <jdhiraj543@gmail.com>
2021-11-19 08:00:55 +0530

    fixed topbar component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,35 +1,32 @@
 import "./topbar.css";
-
-import React from "react";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { useContext } from "react";
-import { AuthContext } from '../../context/AuthContext';
-export default function Topbar() {
+import { AuthContext } from "../../context/AuthContext";
 
+export default function Topbar() {
   const { user } = useContext(AuthContext);
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">gosocial</span>
+          <span className="logo">Lamasocial</span>
         </Link>
       </div>
       <div className="topbarCenter">
-        <div className="searchBar">
+        <div className="searchbar">
           <Search className="searchIcon" />
           <input
-            placeholder="Search for friend, or post"
-            type="text"
+            placeholder="Search for friend, post or video"
             className="searchInput"
           />
         </div>
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
-          <span className="topbarLink">HomePage</span>
-          <span className="topbarLink">TimeLine</span>
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
         </div>
         <div className="topbarIcons">
           <div className="topbarIconItem">
@@ -45,10 +42,18 @@ export default function Topbar() {
             <span className="topbarIconBadge">1</span>
           </div>
         </div>
+        <Link to={`/profile/${user.username}`}>
+          <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
       </div>
-      <Link to={`/profile/${user.username}`}>
-        <img src={user.profilePicture ? PF + user.profilePicture : PF + "profile/noProfile.png"} alt="" className="topbarImg" />
-      </Link>
-    </div >
+    </div>
   );
 }
