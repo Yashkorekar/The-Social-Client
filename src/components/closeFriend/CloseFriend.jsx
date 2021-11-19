5592243a63f72aa3ef8c00b2088754ea3d6bb257
Dhiraj <jdhiraj543@gmail.com>
2021-11-19 07:51:33 +0530

    close Friends updated

--- src/components/closeFriend/CloseFriend.jsx
+++ src/components/closeFriend/CloseFriend.jsx
@@ -1,18 +1,11 @@
 import "./closeFriend.css";
-import { React } from "react";
 
-function CloseFriend({ user }) {
-    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-    return (
-        <li className="sidebarFriend">
-            <img
-                src={PF+user.profilePicture}
-                alt=""
-                className="sidebarFriendImg"
-            />
-            <span className="sidebarFriendName">{user.username}</span>
-        </li>
-    )
+export default function CloseFriend({user}) {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  return (
+    <li className="sidebarFriend">
+      <img className="sidebarFriendImg" src={PF+user.profilePicture} alt="" />
+      <span className="sidebarFriendName">{user.username}</span>
+    </li>
+  );
 }
-
-export default CloseFriend
